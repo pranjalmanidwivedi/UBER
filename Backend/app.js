67234de87bb084d08a6b8ea4acca533d61e53b96
pranjalmanidwivedi <pranjalmanidwivedi@gmail.com>
2025-02-05 14:02:57 +0530

    user authentication done

--- Backend/app.js
+++ Backend/app.js
@@ -3,9 +3,10 @@ dotenv.config();
 const express = require("express");
 const cors = require('cors');
 const app = express();
+const cookieParser = require('cookie-parser');
 const connectToDb = require('./db/db');
 const userRoutes = require('./routes/user.routes')
-
+ 
 connectToDb();
 
 app.use(cors());
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({
     extended: true
 }));
+app.use(cookieParser());
 
 app.get("/", (req, res)=> {
     res.send('hello world');
--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const {body} = require('express-validator');
 const userController = require('../controllers/user.controller');
-
+const authMiddleware = require('../middlewares/auth.middleware');
 
 router.post('/register', [
     body('email').isEmail().withMessage('Invalid Email'),
@@ -14,6 +14,10 @@ router.post('/login', [
     body('email').isEmail().withMessage('Invalid Email'),
     body('password').isLength({ min:6}).withMessage('Incorrect Password')
 ], userController.loginUser)
+
+router.get('/profile', authMiddleware.authUser, userController.getUserProfile)
+
+router.get('/logout', authMiddleware.authUser, userController.logoutUser)
 
 module.exports = router;
 
